68a453074296e3ad3588312d5b1be28388ce18da
qtotuan <quynh.totuan@gmail.com>
2017-05-17 08:43:38 -0400

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,6 @@
+class Artist
+  attr_accessor :name
+  def name=(name)
+    @name = name
+  end
+end
---
+++ lib/author.rb
@@ -0,0 +1,6 @@
+class Author
+  attr_accessor :name
+  def name=(name)
+    @name = name
+  end
+end
